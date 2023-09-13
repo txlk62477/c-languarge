cb59a3fab12d3435832e87200813d01b2a71a976
txlk62477 <143635738+txlk62477@users.noreply.github.com>
2023-09-13 22:38:09 +0800

    Add files via upload

---
+++ c7&9.13/c7&9.13/代码高手.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+	int line = 0;
+	printf("加油，坚持就是胜利\n");
+		while (line < 20)	
+		{
+			printf("长风破浪会有时\t");
+			printf("奥里给:%d\n",line);
+			line = line + 1;
+
+
+
+
+		}
+		if (line >= 20)
+		{
+
+
+			printf("好生活\n");
+
+		}
+	
+	     printf("结束啦");
+
+
+
+
+
+
+
+
+	return 0;
+}
